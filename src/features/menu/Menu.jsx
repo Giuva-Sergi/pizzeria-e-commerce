d50a11bba5a57141e95422079b1d2f51c467426e
Giovanni Sergi <giovanni.srg@gmail.com>
2024-04-10 12:14:48 +0200

    <feat> created menu loader

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,4 +1,15 @@
+import { getMenu } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
 function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+
   return <h1>Menu</h1>;
 }
 
